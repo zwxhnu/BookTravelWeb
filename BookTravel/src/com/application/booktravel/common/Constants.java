8adf02b0af3f84652e40abdd5e84e9e60769563b
zwxhnu <2534948091@qq.com>
2016-05-27 11:39:52 +0800

    by hww 20160527

--- BookTravel/src/com/application/booktravel/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.application.booktravel.common;
-
-public class Constants {
-	// 120.27.37.245
-	public static final String URL = "http://120.27.37.245:2935/BookTravelWeb/";// 服务器ip
-	public static final String SAVE_USER = "saveUser";// 保存用户信息的xml文件
-}
